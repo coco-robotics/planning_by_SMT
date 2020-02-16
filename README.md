5ff76fdeda847d0d935619876f063cc0ce5a12ce
Hejia Zhang <hjzh578@gmail.com>
2020-02-15 21:00:45 -0800

    add task description

--- README.md
+++ README.md
@@ -5,6 +5,20 @@
 Solving PDDL planning problems with a SMT solver. Reference: http://planning.cs.uiuc.edu/booka4.pdf, Section 2.4 Using 
 Logic to Formulate Discrete Planning.
 
-### How to finish the task
+### Set up environment
+- Assume you have a Ubuntu environment (I use Ubuntu 18.04)
 - Clone the repo to your local machine
-- C
+- Create a new branch named with your name
+- Create [virtual env](https://virtualenv.pypa.io/en/latest/) for your project
+- Under the virtual env, install a pddl parser [pddl-lib](https://github.com/hfoffani/pddl-lib), [antlr4-python3-runtime](https://pypi.org/project/antlr4-python3-runtime/). 
+Note here you may need to uninstall defaultly installed antlr4-python3-runtime with pddlpy and install it manually.
+- Install SMT solver [z3](https://github.com/Z3Prover/z3)
+- Run example.py to see the basic usage of pddlpy
+
+### What do you need to implement
+- A python class which can convert the PDDL problem to a logic formula (one file)
+- Call z3 to solve the satisfactory problem for the logic formula (one file)
+
+### Commit and push your code
+- Upload your code to the repo under the branch name you just created
+
---
+++ example.py
@@ -0,0 +1,31 @@
+import os.path as osp
+
+import pddlpy
+
+
+if __name__ == '__main__':
+    root_path = osp.dirname(osp.abspath(__file__))
+
+    pddl_domain_file = osp.join(root_path, 'pddl_files', 'feeding_domain.pddl')
+    pddl_problem_file = osp.join(root_path, 'pddl_files', 'feeding_problem.pddl')
+
+    # Use pddlpy to parse pddl problem definitions
+    domprob = pddlpy.DomainProblem(pddl_domain_file, pddl_problem_file)
+
+    # Print initial states of the pddl problem
+    print("Initial states: ", domprob.initialstate())
+
+    # Print list of operators
+    for operator in domprob.operators():
+        # Print operator's name
+        print("Operator name: {}".format(operator))
+        # Print variable list
+        print("  Variable list: ", domprob.domain.operators[operator].variable_list)
+        # Print positive preconditions
+        print(" Positive preconditions: ", domprob.domain.operators[operator].precondition_pos)
+        # Print negative preconditions
+        print(" Negative preconditions: ", domprob.domain.operators[operator].precondition_neg)
+        # Print positive effects
+        print(" Positive effects: ", domprob.domain.operators[operator].effect_pos)
+        # Print negative effects
+        print(" Negative effects: ", domprob.domain.operators[operator].effect_neg)
